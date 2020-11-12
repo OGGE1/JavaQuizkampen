ef88a4af3eb62c0d0020c097657e50e7d91d9c14
Oscar Norman <oscar.norman95@gmail.com>
2020-11-12 15:21:35 +0100

    cleaned up

--- src/Client.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Hanna Edlund
- * Date: 2020-11-12
- * Time: 13:57
- * Project: JavaQuizkampen
- */
-public class Client {
-}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 /**
  * Created by Oscar Norman <br>
  * Date: 2020-11-12   <br>
- * Time: 13:01   <br>
+ * Time: 15:21   <br>
  * Project: JavaQuizkampen <br>
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hej");
-        System.out.println("Hej");
-    }
 }

0c2a1b1fcec65ba11f3216b446902c94777ba4c8
SanaJava <sana.eneroth@gmail.com>
2020-11-13 00:19:00 +0100

    Add some panels and buttons to the Frame

--- src/UserInterface/GameBoard.java
+++ src/UserInterface/GameBoard.java
@@ -11,13 +11,51 @@ import java.awt.*;
  */
 public class GameBoard extends JFrame {
 
-    public GameBoard(){
+    public GameBoard() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
         this.setSize(300, 500);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        this.getContentPane().setBackground(Color.cyan);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
+        setupLobby(getContentPane());
+        this.setVisible(true);
+    }
 
+    public void setupLobby(Container controlPanel) {
+        controlPanel.setLayout(new BorderLayout());
+
+        createTopPanel(controlPanel);
+
+        createButton(controlPanel);
+    }
+
+    private void createTopPanel(Container container) {
+        ImageIcon imageIcon = new ImageIcon("/Users/sana/Desktop/logo.png");
+        Image image = imageIcon.getImage();
+        Image scaledInstance = image.getScaledInstance(300, 70, Image.SCALE_SMOOTH);
+        imageIcon.setImage(scaledInstance);
+        JLabel iconLogo = new JLabel(imageIcon);
+
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.decode("#4AA8CC"));
+        topPanel.setPreferredSize(new Dimension(300, 80));
+        topPanel.add(iconLogo);
+
+        container.add(topPanel, BorderLayout.NORTH);
+    }
+
+    private void createButton(Container container) {
+        JPanel buttonPanel = new JPanel();
+
+        JButton startNewGame = new JButton("START NEW GAME");
+        startNewGame.setPreferredSize(new Dimension(280, 50));
+        startNewGame.setBackground(Color.decode("#40DA3F"));
+        startNewGame.setOpaque(true);
+        startNewGame.setBorderPainted(false);
+
+        buttonPanel.add(startNewGame);
+        buttonPanel.setOpaque(false);
+
+        container.add(buttonPanel, BorderLayout.SOUTH);
     }
 }

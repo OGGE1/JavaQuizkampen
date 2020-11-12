471fd20128f9fa13dc112ef8cff7e4aedf619cac
Oscar Norman <oscar.norman95@gmail.com>
2020-11-12 13:01:43 +0100

    init source folder

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Oscar Norman <br>
+ * Date: 2020-11-12   <br>
+ * Time: 13:01   <br>
+ * Project: JavaQuizkampen <br>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}

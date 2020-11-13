6d6af094f756f241f1243637a019e72cff189116
Hanna Edlund <hanna@edlunds.biz>
2020-11-13 11:21:19 +0100

    Created a loop for testing the rounds

--- src/Panels/ResultPanel.java
+++ src/Panels/ResultPanel.java
@@ -2,6 +2,9 @@ package Panels;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 
 /**
@@ -13,52 +16,38 @@ import java.util.logging.Level;
 public class ResultPanel extends JFrame {
     String playerOne = "Player one";
     String playerTwo = "Player two";
-    String categoryTitle = "Kategorin";
 
     int playerOneScore = 0;
     int playerTwoScore = 0;
     //TODO: Hur många ronder ska vara default?
     int amountOfRounds =  6;
 
+    String[] categoryList = {"Sport & Fritid", "Djur & Natur", "Jorden runt", "Data- & tvspel"};
+
     JLabel namePlayerOne = new JLabel(playerOne);
     JLabel namePlayerTwo = new JLabel(playerTwo);
-
-
     JLabel score = new JLabel(" " + playerOneScore + " - " + playerTwoScore + " ");
 
-
-
     JPanel backgroundPanel = new JPanel();
-
 
     ResultPanel(){
         score.setFont(new Font("", Font.BOLD, 22));
         namePlayerOne.setFont(new Font("", Font.BOLD, 18));
         namePlayerTwo.setFont(new Font("", Font.BOLD, 18));
 
-
         JPanel topPanel = new JPanel(new BorderLayout());
 
         add(backgroundPanel);
         backgroundPanel.add(topPanel);
-
         topPanel.add(namePlayerOne, BorderLayout.WEST);
         topPanel.add(score, BorderLayout.CENTER);
         topPanel.add(namePlayerTwo, BorderLayout.EAST);
 
-        JPanel scoreBoard = new JPanel(new GridLayout(amountOfRounds,0));
-
-
-
-        newRound(1, "Sport & Fritid");
-        newRound(2, "Djur & Natur");
-        newRound(3, "Jorden runt");
-        newRound(4, "Data & Tvspel");
-        newRound(5, "Test & Testing");
-        newRound(6, "Skog & träd");
-
-
-
+        for (int i = 1; i <= amountOfRounds ; i++) {
+            Random r = new Random();
+            int y = r.nextInt(4);
+            newRound(i, categoryList[y]);
+        }
 
         //setResizable(false);
         setVisible(true);
@@ -73,13 +62,13 @@ public class ResultPanel extends JFrame {
         rond.setFont(new Font("", Font.BOLD, 12));
         category.setFont(new Font("", Font.BOLD, 12));
 
-
         JTextArea ruta = new JTextArea();
         JTextArea ruta2 = new JTextArea();
         JTextArea ruta3 = new JTextArea();
         JTextArea ruta4 = new JTextArea();
         JTextArea ruta5 = new JTextArea();
         JTextArea ruta6 = new JTextArea();
+
         rond.setText("ROND " + rondNr);
         category.setText(categoryTitle);
 
@@ -111,18 +100,14 @@ public class ResultPanel extends JFrame {
         JPanel rightColumn = new JPanel(new GridLayout(0,3));
 
         backgroundPanel.add(leftColumn);
-
 //        leftColumn.add(new JLabel("HEJ"));
         leftColumn.add(ruta);
         leftColumn.add(ruta2);
         leftColumn.add(ruta3);
 
-
-
         backgroundPanel.add(centerColumn);
         centerColumn.add(rond);
         centerColumn.add(category);
-
 
         backgroundPanel.add(rightColumn);
 //        rightColumn.add(new JLabel("DÅ"));

d030ff4cbc62d51da215b2a21c18ef4aba57a60b
Tomas Dahlander <tomas.dahlander@gmail.com>
2020-11-16 16:17:52 +0100

    Setup Database and a textfile standard for reading in from file

--- src/Server/Database.java
+++ src/Server/Database.java
@@ -1,7 +1,13 @@
 package Server;
 
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Tomas Dahlander <br>
@@ -11,31 +17,68 @@ import java.util.List;
  */
 public class Database {
 
-    String[]categories = {"Sport och Fritid","Jorden runt","Djur och Natur","Data & TV-spel"};
+    private String[]categories = {"Sport och Fritid","Jorden runt","Djur och Natur","Data & TV-spel"};
 
+    List<QA> category0 = new ArrayList<>();
     List<QA> category1 = new ArrayList<>();
     List<QA> category2 = new ArrayList<>();
     List<QA> category3 = new ArrayList<>();
-    List<QA> category4 = new ArrayList<>();
 
+    public Database(){
+        loadData(0,"Sport och Fritid.txt");
+    }
+
+    public void loadData(int categoryIndex, String fileName){
+        try(BufferedReader in = new BufferedReader(new FileReader(fileName))){
+            String question;
+
+            while((question = in.readLine()) != null){
+                String correctAnswer = in.readLine();
+                String option0 = in.readLine();
+                String option1 = in.readLine();
+                String option2 = in.readLine();
+                String option3 = in.readLine();
+                String[]optionsArray = {option0,option1,option2,option3};
+
+                if(categoryIndex == 0) category0.add(new QA(question,correctAnswer,optionsArray));
+                else if(categoryIndex == 1) category1.add(new QA(question,correctAnswer,optionsArray));
+                else if(categoryIndex == 2) category2.add(new QA(question,correctAnswer,optionsArray));
+                else category3.add(new QA(question,correctAnswer,optionsArray));
+
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String[] getCategories(){
+        return categories;
+    }
+
+    public List<QA> getQA_List(int categoryIndex){
+        if(categoryIndex == 0) return category0;
+        else if(categoryIndex == 1) return category1;
+        else if(categoryIndex == 3) return category2;
+        else return category3;
+    }
 
     public static void main(String[] args) {
         Database d = new Database();
 
-        String[] array = {"Kalle","John","Anders","Viktor"};
+        for(var i : d.getCategories()){
+            System.out.println(i);
+        }
+        System.out.println();
 
-        d.category1.add(new QA("Vad heter du?","John",array));
-
-        System.out.println("Kategorier***************");
-        for(int i = 0; i < 4; i++){
-            System.out.println(d.categories[i]);
+        for(int i = 0; i < d.category0.size(); i++){
+            System.out.println(d.category0.get(i).getQuestion());
+            System.out.println(d.category0.get(i).getCorrectAnswer());
+            for (int j = 0; j < 4; j++){
+                System.out.println(d.category0.get(i).getOptions()[j]);
+            }
         }
 
-        System.out.println("Fråga: " + d.category1.get(0).getQuestion());
-        System.out.println("Svar:  " + d.category1.get(0).getCorrectAnswer());
 
-        for(int i = 0; i < 4; i++){
-            System.out.println(d.category1.get(0).getOptions()[i]);
-        }
     }
 }
